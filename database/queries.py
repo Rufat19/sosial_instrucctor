b267e4053a8993edb410725331d43e80d8b62636
babayev19 <babayev.rufat.official@gmail.com>
2025-10-22 22:47:34 +0400

    Updated queries.py to use connection pool system

--- database/queries.py
+++ database/queries.py
@@ -1,18 +1,22 @@
-from .db_connect import connect
+# database/queries.py
+from .db_connect import create_pool, pool
 
 # Yeni istifadəçi əlavə et
 async def add_user(user_id: int, name: str, lang: str):
-    conn = await connect()
-    await conn.execute("""
-        INSERT INTO users (user_id, name, lang)
-        VALUES ($1, $2, $3)
-        ON CONFLICT (user_id) DO NOTHING;
-    """, user_id, name, lang)
-    await conn.close()
+    # Əgər pool hələ yaradılmayıbsa, yaradılır
+    if pool is None:
+        await create_pool()
+    async with pool.acquire() as conn:
+        await conn.execute("""
+            INSERT INTO users (user_id, name, lang)
+            VALUES ($1, $2, $3)
+            ON CONFLICT (user_id) DO NOTHING;
+        """, user_id, name, lang)
 
 # Bütün istifadəçiləri göstər
 async def get_all_users():
-    conn = await connect()
-    rows = await conn.fetch("SELECT * FROM users;")
-    await conn.close()
-    return rows
+    if pool is None:
+        await create_pool()
+    async with pool.acquire() as conn:
+        rows = await conn.fetch("SELECT * FROM users;")
+        return rows
